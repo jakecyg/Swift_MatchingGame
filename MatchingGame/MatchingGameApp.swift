2d3e7664c3bde0858a188854b8041c8f4272da25
jakecyg <43617272+jakecyg@users.noreply.github.com>
2020-11-19 00:10:01 -0600

    Initial Commit

---
+++ MatchingGame/MatchingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchingGameApp.swift
+//  MatchingGame
+//
+//  Created by Jake Choi on 2020-11-19.
+//
+
+import SwiftUI
+
+@main
+struct MatchingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
